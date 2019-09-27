8018ec877bb7cebf4fc0301e46a46c499d7a6a6c
annithinggoes <acha818@aucklanduni.ac.nz>
2019-09-27 14:23:20 +1200

    Fixed video player closing

--- Wiki_Speak_GUI/src/application/VideoCreation.java
+++ Wiki_Speak_GUI/src/application/VideoCreation.java
@@ -94,9 +94,10 @@ public class VideoCreation {
 		String creation = "\"Creations" + File.separatorChar + name + ".mp4\"";
 
 		try {
-			retrieveImages(term, 5);
-			String cmd = "ffmpeg -y -r `ls "+term+"*.jpg | wc -l`/`soxi -D " + audio + "` -i " + term + "%02d.jpg -vf \"scale=1024:720:force_original_aspect_ratio=decrease,pad=1024:720:(ow-iw)/2:(oh-ih)/2,drawtext=FreeSerif.ttf:fontsize=50: "
-					+ "fontcolor=white:x=(w-text_w)/2:y=(h-text_h)/2:text="+term+"\" -c:v libx264 -pix_fmt yuv420p " + video + ";ffmpeg -i " + video + " -i " + audio + " -c:v copy -c:a aac -strict experimental "+ creation;
+			retrieveImages(term, numOfImages);
+			String cmd = "ffmpeg -y -r `ls "+term+"*.jpg | wc -l`/`soxi -D " + audio + "` -i " + term + "%02d.jpg -vf \"scale=1024:720:force_original_aspect_ratio=decrease,"
+					+ "pad=1024:720:(ow-iw)/2:(oh-ih)/2,drawtext=FreeSerif.ttf:fontsize=50: fontcolor=white:x=(w-text_w)/2:y=(h-text_h)/2:text=" + term + "\" -c:v libx264 "
+							+ "-pix_fmt yuv420p " + video + ";ffmpeg -i " + video + " -i " + audio + " -c:v copy -c:a aac -strict experimental "+ creation + "; rm "+term+ "*.jpg";
 			ProcessBuilder pb = new ProcessBuilder("bash", "-c", cmd);
 			Process creationProcess = pb.start();
 
