28127bd6b1b2a1d2b0b2ba68ac0bcc12e0d4dfd3
Donghyun Lee <dlee658@en-369724.uoa.auckland.ac.nz>
2019-10-09 14:52:10 +1300

    added some new packages

---
+++ Wiki_Speak_GUI/src/controller/QuizController.java
@@ -0,0 +1,22 @@
+package controller;
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+
+public class QuizController {
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
