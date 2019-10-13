7398ae89c4d68d558e3884c492d7871563031b04
Donghyun Lee <dlee658@en-337635.uoa.auckland.ac.nz>
2019-10-13 23:25:31 +1300

    modified

--- Wiki_Speak_GUI/src/controller/ImageRetrieveController.java
+++ Wiki_Speak_GUI/src/controller/ImageRetrieveController.java
@@ -3,6 +3,7 @@ package controller;
 import java.awt.image.BufferedImage;
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
@@ -25,6 +26,8 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ProgressIndicator;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 public class ImageRetrieveController {
 	@FXML 
@@ -42,6 +45,29 @@ public class ImageRetrieveController {
 	@FXML 
 	private ProgressIndicator imageIndicator;
 	
+	@FXML
+	private ImageView image1;
+	
+	@FXML
+	private ImageView image2;
+	@FXML
+	private ImageView image3;
+	@FXML
+	private ImageView image4;
+	@FXML
+	private ImageView image5;
+	@FXML
+	private ImageView image6;
+	@FXML
+	private ImageView image7;
+	@FXML
+	private ImageView image8;
+	@FXML
+	private ImageView image9;
+	
+	@FXML
+	private ImageView image10;
+	
 	@FXML 
 	private ChoiceBox<Integer> imageCB;
 
@@ -54,6 +80,26 @@ public class ImageRetrieveController {
 		imageCB.setValue(1);
 		imageCB.getItems().addAll(1,2,3,4,5,6,7,8,9,10);
 		BufferedReader reader;
+
+		
+		///
+		
+
+
+		image1.setImage(null);
+		image2.setImage(null);
+		image3.setImage(null);
+		image4.setImage(null);
+		image5.setImage(null);
+		image6.setImage(null);
+		image7.setImage(null);
+		image8.setImage(null);
+		image9.setImage(null);
+		image10.setImage(null);
+		
+		
+		
+		
 		try {
 			reader = new BufferedReader(new FileReader("term.txt"));
 			term = reader.readLine();
@@ -65,7 +111,6 @@ public class ImageRetrieveController {
 		}
 
 	}
-	
 	
 	@FXML
 	private void handleHomeBtnAction(ActionEvent event) {
@@ -103,6 +148,10 @@ public class ImageRetrieveController {
 	
 	@FXML
 	private void handleBackBtnAction(ActionEvent event) {
+		
+
+		
+		
 		try {	
 			FXMLLoader loader = new FXMLLoader();
 			loader.setLocation(Main.class.getResource("audioView.fxml"));
@@ -130,12 +179,13 @@ public class ImageRetrieveController {
 		throw new RuntimeException("Couldn't find " + key +" in config file "+file.getName());
 	}
 
+	
 	private void retrieveImages(String term, int numOfImages) {
 		try {
-			String apiKey = getAPIKey("apiKey");
-			String sharedSecret = getAPIKey("sharedSecret");
+		//	String apiKey = getAPIKey("apiKey");
+		//	String sharedSecret = getAPIKey("sharedSecret");
 
-			Flickr flickr = new Flickr(apiKey, sharedSecret, new REST());
+			Flickr flickr = new Flickr("0677640e0175232aebd8e54ec482ec04", "b135e20999eff6c5", new REST());
 
 			int page = 0;
 
@@ -154,8 +204,28 @@ public class ImageRetrieveController {
 					String filename;
 					if (i == 10) {
 						filename = term+i+".jpg";
+						
+						
+						
 					} else {
 						filename = term+"0"+i+".jpg";
+						
+						if (i==0) {
+							try {
+								//might need to change
+								String cwd = System.getProperty("user.dir");
+								FileInputStream input;
+								//display the image
+								input = new FileInputStream(cwd+"/"+term+"01.jpg");
+								Image ima1 = new Image(input);
+								image1.setImage(ima1);
+							} catch (FileNotFoundException e1) {
+								// TODO Auto-generated catch block
+								e1.printStackTrace();
+							}
+						}
+						
+						
 					}			
 					File outputfile = new File(filename);
 					ImageIO.write(image, "jpg", outputfile);
