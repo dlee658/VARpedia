21b7607e2bfa124048fe964ad0956b18dc691916
annithinggoes <acha818@aucklanduni.ac.nz>
2019-10-24 10:42:46 +1300

    fixed textless video bug

--- Wiki_Speak_GUI/src/application/VideoCreation.java
+++ Wiki_Speak_GUI/src/application/VideoCreation.java
@@ -17,8 +17,9 @@ public class VideoCreation {
 		try {	
 						
 			//Create textless creation
-			String cmd = "cat Image\\*.jpg | ffmpeg -f image2pipe -framerate " + numOfImages + "/`soxi -D " + audio + "` -i - -i "+ audio + " -c:v libx264 -pix_fmt yuv420p -vf \"scale=640:480:force_original_aspect_ratio=decrease,"
+			String cmd = "cat *.jpg | ffmpeg -f image2pipe -framerate " + numOfImages + "/$(soxi -D " + audio + ") -i - -i "+ audio + " -c:v libx264 -pix_fmt yuv420p -vf \"scale=640:480:force_original_aspect_ratio=decrease,"
 					+ "pad=640:480:(ow-iw)/2:(oh-ih)/2\" -r 25 -max_muxing_queue_size 1024 -y " + video + " &> status.txt;"; 
+			
 			//Create creation
 			String cmd2 = "ffmpeg -i "+ video + " -c:v libx264 -pix_fmt yuv420p -vf \"drawtext=FreeSerif.ttf:fontsize=50: fontcolor=white:x=(w-text_w)/2:y=(h-text_h)/2:text=" + term + "\" -r 25 -max_muxing_queue_size 1024 -y " + creation  +" &> status2.txt"; 
 			
